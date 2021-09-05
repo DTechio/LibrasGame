4942ee51b24cfb352852b1f3b7437ee8cdae93f8
Daniel Techio <danieltechio@hotmail.com>
2021-09-04 23:11:35 -0300

    Puzzle Drag&Drop
    The Bug is on the table

---
+++ LibrasGame/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private RectTransform _transform;
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private CanvasGroup _canvasGroup;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _canvasGroup.blocksRaycasts = false;
+        _canvasGroup.alpha = 0.5f;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _transform.anchoredPosition += eventData.delta; // / _canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _canvasGroup.blocksRaycasts = true;
+        _canvasGroup.alpha = 1f;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Apertou");
+    }
+}
---
+++ LibrasGame/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    [SerializeField] private RectTransform _transform;
+    public void OnDrop(PointerEventData eventData)
+    {
+        eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = _transform.anchoredPosition;
+    }
+}
